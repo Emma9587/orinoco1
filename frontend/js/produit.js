ed45c57dbb45d53c2fa9120a8fb2fbc6b13d5acc
Emmanuelle Normand <angel_kamael@hotmail.fr>
2020-12-16 14:10:08 +0100

    16/12
    mise en forme javascript page produit

--- frontend/js/produit.js
+++ frontend/js/produit.js
@@ -1,10 +1,12 @@
-// Recupere idproduit de url
-let idProduit = ;
+// Recupere idproduit de url (Comment récupérer une variable url en JS)
+// console.log(document.location);
+
+let searchParams = new URLSearchParams(document.location.search);
+let idProduit = searchParams.get("idproduit");
+console.log(idProduit);
+
 let url = 'http://localhost:3000/api/teddies/'+idProduit;
-
-
 //Envoyé idproduit dans le fétch
-
 fetch (url, {
     method: 'GET'
 }).then((data) =>{
@@ -13,18 +15,23 @@ fetch (url, {
     console.log(produit);
 
     // Afficher les informaltion du produit dans le html
-
     let myHTML = '';
-
-    produit.forEach((produit) =>{
-        console.log(produit.name);
+    console.log(produit.name);
         console.log(produit.price);
         console.log(produit.imageUrl);
-        console.log(produit.description);
-        myHTML += `<div class="col-12 col-md-5 col-lg-6"><div class="card" id="${produit.name}"><div class="card-body"><h2>${produit.name}</h2><img src="${produit.imageUrl}"><p>${produit.price}€</p><p>${produit.description}</p><a href="http://localhost:3000/api/teddies/_id"></a></div></div></div>`;
-        
-    })
-    console.log(myHTML)
+        console.log(produit.description);   
+        myHTML += `<div class="col-12 col-md-5 col-lg-6"><div class="card" id="${produit.name}"><div class="card-body"><h2>${produit.name}</h2><img src="${produit.imageUrl}"><p>${produit.price}€</p><button class="btn"><a href="pages/produit.html?idproduit=${produit._id}" class="border-0" >Personnaliser</a></button></div></div></div>`;
 
-    $('#teddie').html(myHTML);
+ console.log(myHTML)
+     $('#teddie').html(myHTML);
+
+    // let myHTML = '';
+    // produit.forEach((produit) =>{
+    //     console.log(produit.name);
+    //     console.log(produit.price);
+    //     console.log(produit.imageUrl);
+    //     console.log(produit.description);
+    //     myHTML += `<div class="col-12 col-md-5 col-lg-6"><div class="card" id="${produit.name}"><div class="card-body"><h2>${produit.name}</h2><img src="${produit.imageUrl}"><p>${produit.price}€</p><p>${produit.description}</p><a href="http://localhost:3000/api/teddies/_id"></a></div></div></div>`;       
+    // })
+    //
 })
