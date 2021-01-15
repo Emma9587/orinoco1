663ac098f05b989f982309227d7fab597343c43a
Emmanuelle Normand <angel_kamael@hotmail.fr>
2021-01-15 14:52:53 +0100

    15/01
    ajout panier page produit

--- frontend/js/panier.js
+++ frontend/js/panier.js
@@ -1,39 +1,74 @@
-let searchParams = new URLSearchParams(document.location.search);
-let idProduit = searchParams.get("idproduit");
-console.log(idProduit);
+// let searchParams = new URLSearchParams(document.location.search);
+// let idProduit = searchParams.get("idproduit");
+// console.log(idProduit);
 
-let url = 'http://localhost:3000/api/teddies/'+idProduit;
-//Envoyé idproduit dans le fétch
-fetch (url, {
-    method: 'GET'
-}).then((data) =>{
-    return data.json();
-}).then((produit) =>{
-    console.log(produit);
+// let url = 'http://localhost:3000/api/teddies/'+idProduit;
+// //Envoyé idproduit dans le fétch
+// fetch (url, {
+//     method: 'GET'
+// }).then((data) =>{
+//     return data.json();
+// }).then((produit) =>{
+//     console.log(produit);
 
-  // Afficher les informaltion du produit dans le html
-  let myHTML = '';
-  console.log(produit.name);
-  console.log(produit.imageUrl);
-  console.log(produit.description);
-  myHTML += `<div class="col-12 col-md-5 col-lg-6"><div class="card" id="${produit.name}"><div class="card-body"><h2 style="padding-top:20px;">${produit.name}</h2><img src="${produit.imageUrl}"><p style="margin-left:20px; margin-right:20px;">${produit.description}</p></div></div></div>`;
+//   // Afficher les informaltion du produit dans le html
+//   let myHTML = '';
+//   console.log(produit.name);
+//   console.log(produit.imageUrl);
+//   console.log(produit.description);
+//   myHTML += `<div class="col-12 col-md-5 col-lg-6"><div class="card" id="${produit.name}"><div class="card-body"><h2 style="padding-top:20px;">${produit.name}</h2><img src="${produit.imageUrl}"><p style="margin-left:20px; margin-right:20px;">${produit.description}</p></div></div></div>`;
 
-  console.log(myHTML)
-  $('#produit').html(myHTML);
+//   console.log(myHTML)
+//   $('#article').html(myHTML);
 
-console.log(produit.price);
- console.log(myHTML)
-  $('#price').html(produit.price);
-
-/*
-  console.log(myHTML)
-    $('#teddie').html(myHTML);
-*/ 
+// console.log(produit.price);
+//  console.log(myHTML)
+//   $('#price').html(produit.price);
 
 
-localStorage.setItem(url, idProduit);
-let myProduit = localStorage.getItem(url, idProduit);
-myProduit
+localStorage.setItem(CardProduits);
+let myProduit = localStorage.getItem(CardProduits);
 console.log(myProduit);
+// myProduit
 
+// });
+
+
+
+
+
+
+//Je récupere les informations de mon panier
+fetch (myProduit, {
+    method: 'GET'
+}).then((data)=>{
+    return data.json();
+}).then((CardProduits) => {
+    console.log(CardProduits);
 });
+let CardProduits = [''];
+CardProduits = JSON.parse(localtorage.getItem('panier'))
+//Le code pour créer le HTML, similaire au code de la page index
+CartProduits.forEach( produit => {let myHTML = '';
+produits.forEach((produit) =>{
+    console.log(produit.name);
+    console.log(produit.price);
+    console.log(produit.imageUrl);
+    myHTML += `<div class="col-12 col-md-5 col-lg-6"><div class="card" id="${produit.name}"><div class="card-body"><h2>${produit.name}</h2><img src="${produit.imageUrl}"><p>${produit.price}€</p><button class="btn"><a href="pages/produit.html?idproduit=${produit._id}" class="border-0" >Personnaliser</a></button></div></div></div>`;
+});
+
+console.log(myHTML);
+
+$('#mesproduits').html(myHTML);
+
+})
+//Je calcule le prix total
+//})
+//J'ajoute le HTML au dom a la page web pour afficher les produits
+
+//Je blocque l'envois du formulaire
+//Je verifie tous les champs (Exemple : email est bien une email contient @)
+
+// Si tous les champs sont ok envoyer via la méthode POST le formulaire au BACKEND (/order)
+//Créer un tableau avec le contact et les produit id
+//Récupérer les informations que renvoie le backend pour les afficher sur la page Confirmaiton de commande
--- frontend/js/produit.js
+++ frontend/js/produit.js
@@ -22,12 +22,56 @@ fetch (url, {
         console.log(produit.description);   
         myHTML += `<div class="col-12 col-md-5 col-lg-6"><div class="card" id="${produit.name}"><div class="card-body"><h2 style="padding-top:20px;">${produit.name}</h2><img src="${produit.imageUrl}"><p style="margin-left:20px; margin-right:20px;">${produit.description}</p><p style="padding-bottom:20px;">${produit.price}€</p></div></div></div>`;
       
-    myHTML += `<div class="col-12 col-md-5 col-lg-6"><button class="btn"><a href="panier.html?idproduit=${produit._id}" class="border-0" >Personnaliser</a></button></div>`;  
-console.log(myHTML)
+    myHTML += `<div class="col-12 col-md-5 col-lg-6"><button class="btn"><a href="panier.html" class="add-to-card" id="ajouterAuPanier">Ajouter au panier</a></button></div>`;  
+    console.log(myHTML)
     $('#teddie').html(myHTML);
 
-   
-    localStorage.setItem(url, idProduit)
-    console.log(localStorage);
+    //Quand je clique je rajoute au panier 
+    let btnpanier = document.getElementById("ajouterAuPanier");
+    console.log(btnpanier);
 
+    btnpanier.addEventListener("click", event => {
+        event.preventDefault();
+        console.log("click ok");
+        
+        myProduit = {
+            id : produit._id,
+            name : produit.name,
+            imageUrl : produit.imageUrl,
+            description : produit.description,
+            price : produit.price,
+        };
+        //si j'ai un panier (localsorage) - Je récupere les information du panier pour pouvoir ajouter les nouveaux produit au panier 
+        let hasPanier = localStorage.getItem('panier')
+        if(hasPanier){
+            //J'ai un panier
+            hasPanier = JSON.parse(CardProduits)
+            CardProduits = 
+            [ 
+                ...hasPanier,
+                myProduit 
+            ]
+        }else{
+            //Je n'ai pas encore de panier
+            CardProduits = [ myProduit ]
+        }
+
+    // function addProduit() {
+    //     if(localStorage >= 1) {   
+    //     }
+    // };
+        console.log(localStorage);
+        //Je convertie le JSON en Varaible utilisable JS
+        //let myProduit = CardProduits;
+        // CartProduits = JSON.parse(localStorage.getItem('panier'))
+
+        //Créer un tableau dans le localstorage
+        
+        //Je transforme mon tableau en JSON
+        CardProduits = JSON.stringify(CardProduits);
+        //Mettre les information que tu as besoins
+        localStorage.setItem('panier', CardProduits);
+        //Fin du click
+        console.log(localStorage.getItem('panier'));
+    });
 });
